4812f09ec68587f66cb678105796966353e68067
yolandamatiwane <mbalentle.matiwane21@gmail.com>
2024-08-29 01:15:03 -0700

    created frontend

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,11 @@
 import { createStore } from 'vuex'
+import axios from 'axios'
+import {useCookies} from 'vue-cookies'
+import router from '@/router'
+/*eslint-disable */
 
+axios.defaults.withCredentials = true
+axios.defaults.headers = $cookies.get('token')
 export default createStore({
   state: {
   },
