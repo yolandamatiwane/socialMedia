224dfa8280aef678a5d017271bfdbba4c58a0bb0
yolandamatiwane <mbalentle.matiwane21@gmail.com>
2024-08-29 01:08:22 -0700

    verify

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -8,26 +8,56 @@ config()
 const checkUser = async (req,res,next)=>{
     const {email,password} = req.body
     if(!email||!password){
-        return res.send("Missing required fields")
+        return res.status(400).json({err:"Missing required fields"})
     }
-    let hashedPassword = (await logInDb(email)).password
-    console.log(hashedPassword)
-    let result = await compare(password,hashedPassword)
+    try {
+        let user = await logInDb(email)
+        console.log(user)
+        if(!user){
+           return res.status(400).json({message:"invalid email"})
+        }
 
-    if(result){
-        let token = jwt.sign({
-            email,
-            password
-        },
-        process.env.SECRET_KEY,
-        {
-            expiresIn:'1h'
-        })
-        req.body.token = token
-        next()
-    } else{
-        res.send("Invalid email or password")
+        let hashedPassword = user.password
+
+        let result = await compare(password,hashedPassword)
+    
+        if(result){
+            let token = jwt.sign({
+                email,
+                password
+            },
+            process.env.SECRET_KEY,
+            {
+                expiresIn:'1h'
+            })
+            req.body.token = token
+            res.status(202).json({token})
+            next()
+        } else{
+            res.status(400).json({err:"Invalid email or password"})
+        }
+    } catch (err) {
+        res.status(500).json({ err: "Internal server error" })
     }
 }
 
-export {checkUser}
+const verifyAToken = (req,res,next)=>{
+    let {cookies} = req.headers
+    // checks if token exists first
+    let token = cookies && cookies.split(' ')[1] // if there is a cookie, then we can split it
+    console.log(req.headers)
+    if(!token){
+        return res.status(401).json({message:"Unauthorized"})
+    }
+    jwt.verify(token,process.env.SECRET_KEY,(err,decoded)=>{
+        if(err){
+            res.json({message:'Token is invalid'})
+            return
+        }
+        // req.body.username = decoded.username
+        req.body.user = decoded.email
+        //  console.log(decoded)
+        next()
+    })
+}
+export {checkUser,verifyAToken}
--- routes/postsRouter.js
+++ routes/postsRouter.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import { fetchPosts, fetchSinglePost, addPost, removePost, updatePost} from '../controller/postsController.js'
+import { verifyAToken } from '../middleware/authenticate.js'
 
 const router = express.Router()
 
 router.get('/',fetchPosts)
 router.get('/:id',fetchSinglePost)
-router.post('/add',addPost)
+router.post('/add',verifyAToken,addPost)
 router.delete('/delete/:id',removePost)
 router.patch('/update/:id', updatePost)
+
 export default router
